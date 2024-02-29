90e02bc047773cd862c048e26a30806be1180bde
navalm7432 <navaljian7432@gmail.com>
2024-02-29 12:39:43 +0530

    vercel deployment config

--- api/server.js
+++ api/server.js
@@ -1,3 +1,50 @@
-import app from "../server"
+// All the import stuffs
+import express from "express";
+import mongoose from "mongoose";
+import items from "./routes/api/items.js";
+import users from "./routes/api/usersRegistration.js";
+import auth from "./routes/api/userAuthorization.js";
+import request from "./routes/api/request.js";
+import swap from "./routes/api/swapRoutes.js";
+import cors from "cors";
+import config from "config";
+import path from "path";
 
-export default app
+// configuring our express framework
+const app = express();
+
+// Middlewares = methods/functions/operations that are called BETWEEN processing the Request and sending the Response in our application method.
+app.use(express.json()); // it is for POST and PUT requests, because in both these requests we are sending data (in the form of some data object) to the server and we are asking the server to accept or store that data (object), which is enclosed in the body (i.e. req.body) of that (POST or PUT) Request
+app.use(cors()); // Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any other origins (domain, scheme, or port) than its own from which a browser should permit loading of resources.
+app.use("/api/items", items); // it basically means that anything that starts with "/api/items" should look into items file which we imported
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/request", request);
+app.use("/api/swapreq", swap);
+// app.use(express.static(path.join(__dirname, "client", "build")));
+
+//DB Config
+const dbURI = config.get("mongoURI"); //passed the mongodb url  for connection
+
+mongoose
+  .connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log("DB is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Serve static  assetsif in production
+// if (process.env.NODE_ENV === "production") {
+//   // Sttaic folder
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname,"client","build","index.html"));
+//   });
+// }
+
+const port = process.env.PORT || 4000; // for devlopment using port:5000 but when deployed it will use process.env.PORT
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+export default app;
